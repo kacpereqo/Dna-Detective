6796c530ca786028e556d877256a5603f9e3371c
kacpereqqo <remzak.k@gmail.com>
2023-01-02 00:01:14 +0100

    created Statistics count functionality

--- app/statistics/routes.py
+++ app/statistics/routes.py
@@ -25,3 +25,43 @@ def get_percentage_of_all_amine_acids(protein: Protein):
     protein  = ProteinStatistics(protein.sequence)
     percentage = protein.percentage_of_all()
     return percentage
+
+#|------------------------------------------------------------------------------|#
+
+@router.post("/api/percentage/charged", tags=["Statistics"], description="Returns flexibility of protein")
+def get_percentage_of_charged_amine_acids(protein: Protein):
+    """Returns flexibility of protein in units""" 
+    protein  = ProteinStatistics(protein.sequence)
+    percentage = protein.percentage_of_charged()
+    return percentage
+
+#|------------------------------------------------------------------------------|#
+
+@router.post("/api/count", tags=["Statistics"], description="Returns flexibility of protein")
+def get_count_of_all_amine_acids(protein: Protein):
+    """Returns flexibility of protein in units""" 
+    protein  = ProteinStatistics(protein.sequence)
+    count = protein.count_of_all()
+    return count
+
+#|------------------------------------------------------------------------------|#
+
+@router.post("/api/count/charged", tags=["Statistics"], description="Returns flexibility of protein")
+def get_count_of_charged_amine_acids(protein: Protein):
+    """Returns flexibility of protein in units""" 
+    protein  = ProteinStatistics(protein.sequence)
+    count = protein.count_of_charged()
+    return count
+
+#|------------------------------------------------------------------------------|#
+
+@router.post("/api/count/{amine_acid}", tags=["Statistics"], description="Returns flexibility of protein")
+def get_count_of_amine_acid(protein: Protein, amine_acid: str):
+    """Returns flexibility of protein in units""" 
+    protein  = ProteinStatistics(protein.sequence)
+    count = protein.count_of(amine_acid)
+    return {amine_acid:count}
+
+#|------------------------------------------------------------------------------|#
+
+
--- app/statistics/service.py
+++ app/statistics/service.py
@@ -13,3 +13,33 @@ class ProteinStatistics():
     def percentage_of_all(self) -> dict:
         """Returns the percentage of all amino acids in the protein"""
         return {amino_acid: self.perctage_of(amino_acid) for amino_acid in self.protein}
+
+#|------------------------------------------------------------------------------|#
+
+    def count_of(self, amino_acid: str) -> int:
+        """Returns the count of a given amino acid in the protein"""
+        return self.protein.count(amino_acid)
+
+#|------------------------------------------------------------------------------|#
+
+    def count_of_all(self) -> dict:
+        """Returns the count of all amino acids in the protein"""
+        return {amino_acid: self.count_of(amino_acid) for amino_acid in self.protein}
+
+#|------------------------------------------------------------------------------|#
+
+    def percentage_of_charged(self) -> dict:
+        """Returns the percentage of charged amino acids in the protein"""
+        charged = ["R", "H", "K", "D", "E"]
+        return {amino_acid: self.perctage_of(amino_acid) for amino_acid in charged}
+
+#|------------------------------------------------------------------------------|#
+
+    def count_of_charged(self) -> dict:
+        """Returns the count of charged amino acids in the protein"""
+        charged = ["R", "H", "K", "D", "E"]
+        return {amino_acid: self.count_of(amino_acid) for amino_acid in charged}
+
+#|------------------------------------------------------------------------------|#
+
+    
