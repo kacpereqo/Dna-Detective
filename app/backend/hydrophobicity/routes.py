ba9a6282eddd5510da66d1988a4174ccedabf020
kacpereqqo <remzak.k@gmail.com>
2023-01-19 17:51:05 +0100

    folder refactor

--- app/backend/hydrophobicity/routes.py
+++ app/backend/hydrophobicity/routes.py
@@ -6,29 +6,28 @@ from fastapi.responses import PlainTextResponse
 from .service import ProteinHydrophobicity
 
 
-
-#|------------------------------------------------------------------------------|#
+# |------------------------------------------------------------------------------|#
 
 router = APIRouter()
 
-#|------------------------------------------------------------------------------|#
+# |------------------------------------------------------------------------------|#
+
 
 @router.post("/api/averagehydrophobicity", tags=["Hydrophobicity"], description="Returns average hydrophobicity of protein")
 def get_hydrophobicity_of_protein(protein: Protein, hydrophobicity_scale: HydrophobicityScale = Depends()):
     """Returns hydrophobicity of protein in units"""
-    
-    protein = ProteinHydrophobicity(protein.sequence)
-    return {"hydrophobicity":round(protein.hydrophobicity(scale = hydrophobicity_scale.scale), 2)}
 
-#|------------------------------------------------------------------------------|#
+    protein = ProteinHydrophobicity(protein.sequence)
+    return {"hydrophobicity": round(protein.hydrophobicity(scale=hydrophobicity_scale.scale), 2)}
+
+# |------------------------------------------------------------------------------|#
+
 
 @router.post("/api/hydrophobicity", tags=["Hydrophobicity"], description="Returns hydrophobicity of each amino acid in protein")
 def get_hydrophobicity_of_protein(protein: Protein, hydrophobicity_scale: HydrophobicityScale = Depends()):
     """Returns hydrophobicity of protein in units"""
 
-    sequence = SequenceParameters(protein.sequence)
-    hydrophobicity = sequence.get_linear_hydropathy()
-    response = dict(enumerate(hydrophobicity.flatten(), 1))
-    return {"hydrophobicity":response}
+    protein = ProteinHydrophobicity(protein.sequence)
+    return {"hydrophobicity": protein.hydrophobicity(scale=hydrophobicity_scale.scale)}
 
-#|------------------------------------------------------------------------------|#
+# |------------------------------------------------------------------------------|#
